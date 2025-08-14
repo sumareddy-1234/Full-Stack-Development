2887817691fa9f46b59b4162379c972798338b2c
Satti Suma <154655364+sumareddy-1234@users.noreply.github.com>
2025-08-14 12:04:52 +0530

    Add files via upload

---
+++ React/FunctionComponent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const FunctionComponent=()=>{
+    var x=10;
+    console.log(x);
+    return (
+        <h1 style={{color:"red"}}>Function Component</h1>
+    );
+}
+export default FunctionComponent;
---
+++ React/MyFunction.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./MyFunction.css";
+function MyFunction() {
+    var x=10;
+    console.log(x);
+    return (
+        <>
+            <h1>Circle</h1>
+            <div className="circle">
+                <h1 className="learn">Learn More</h1>
+            </div>
+        </>
+    );
+}
+export default MyFunction;
