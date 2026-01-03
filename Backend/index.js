75d5f52387be8c4cab59781976c2550165682752
Satti Suma <154655364+sumareddy-1234@users.noreply.github.com>
2026-01-03 10:46:53 +0530

    To start Server

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,6 @@
-import http from 'http';
-const server=http.createServer((req,res)=>{
-    console.log(req.method);
-    if(req.url=="/users"){
-        res.writeHead(200,{"Content-Type":"application/json"});
-        res.end(JSON.stringify({name:"suma"}))
-    }else{
-        res.writeHead(200,{"contenr-Type":"text/plain"});
-        res.end("Hello this is from backend")
-    }
-});
-server.listen(7007,()=>{
-    console.log(`server running at port ${7007}`)
+const express=require("express")
+const app=express()
+
+app.listen(9000, () => {
+    console.log("server Started at port 9000")
 });
