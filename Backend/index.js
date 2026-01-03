fb7a0ee6d07d86d17c44ac89cd748b8e171b1fe0
Satti Suma <154655364+sumareddy-1234@users.noreply.github.com>
2026-01-03 11:47:37 +0530

    Add files via upload

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const app=express()
-
+const FirstRoute=require("./routers/FirstRoute")
+app.use("/",FirstRoute);
 app.listen(9000, () => {
     console.log("server Started at port 9000")
 });
